4c8e1b8a4197a8191cc179b59377e03485779a6c
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2022-02-01 19:45:46 +0800

    pdf

--- admin/all_orders.php
+++ admin/all_orders.php
@@ -4,6 +4,70 @@
 include("../connection/connect.php");
 error_reporting(0);
 session_start();
+ob_start();
+
+function fetch_data()  
+ {  
+      $output = '';  
+      $servername = "localhost"; //server
+$username = "root"; //username
+$password = ""; //password
+$dbname = "online_rest";  //database
+      $db = mysqli_connect($servername, $username, $password, $dbname);
+      $sql = "SELECT users.*, users_orders.* FROM users INNER JOIN users_orders ON users.u_id=users_orders.u_id ORDER BY o_id ASC";  
+      $result = mysqli_query($db, $sql);  
+      while($rows = mysqli_fetch_array($result))  
+      {       
+      $output .= '<tr>   
+                    <td>'.$rows['o_id'].'</td>                         
+                    <td>'.$rows['username'].'</td>
+                    <td>'.$rows['title'].'</td>
+                    <td>'.$rows['quantity'].'</td>
+                    <td>$'.$rows['price'].'</td>
+                    <td>$'.$rows['price']*$rows['quantity'].'</td>
+                    <td>'.$rows['address'].'</td>
+                    <td>'.$rows['date'].'</td>
+                </tr>';  
+      }  
+      return $output;  
+ }  
+if(isset($_POST["create_pdf"]))  
+ {  
+      require_once('tcpdf/tcpdf.php');  
+      $obj_pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);  
+      $obj_pdf->SetCreator(PDF_CREATOR);  
+      $obj_pdf->SetTitle("Zinnovare's Orders Report");  
+      $obj_pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);  
+      $obj_pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));  
+      $obj_pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));  
+      $obj_pdf->SetDefaultMonospacedFont('helvetica');  
+      $obj_pdf->SetFooterMargin(PDF_MARGIN_FOOTER);  
+      $obj_pdf->SetMargins(PDF_MARGIN_LEFT, '15', PDF_MARGIN_RIGHT);  
+      $obj_pdf->setPrintHeader(false);  
+      $obj_pdf->setPrintFooter(false);  
+      $obj_pdf->SetAutoPageBreak(TRUE, 10);  
+      $obj_pdf->SetFont('helvetica', '', 10);  
+      $obj_pdf->AddPage();  
+      $content = '';  
+      $content .= '  
+      <h3 align="center">Zinnovare\'s Orders Report</h3><br /><br />  
+      <table border="1" cellspacing="0" cellpadding="5">  
+           <tr>  
+                <th width="5%">ID</th>
+                <th width="15%">Username</th>
+                <th width="20%">Title</th>
+                <th width="10%">Quantity</th>  
+                <th width="10%">Price</th>  
+                <th width="10%">Total</th>  
+                <th width="18%">Address</th>
+                <th width="12%">Date</th>
+           </tr>  
+      ';  
+      $content .= fetch_data();  
+      $content .= '</table>';  
+      $obj_pdf->writeHTML($content);  
+      $obj_pdf->Output('sample.pdf', 'I');  
+ } 
 
 ?>
 <head>
@@ -266,6 +330,10 @@ session_start();
                                            
                                         </tbody>
                                     </table>
+                                    <br />  
+                     <form method="post">  
+                          <input type="submit" name="create_pdf" class="btn btn-danger" value="Download Report" />  
+                     </form> 
                                 </div>
                             </div>
                         </div>
--- admin/allusers.php
+++ admin/allusers.php
@@ -4,6 +4,70 @@
 include("../connection/connect.php");
 error_reporting(0);
 session_start();
+ob_start();
+
+function fetch_data()  
+ {  
+      $output = '';  
+      $servername = "localhost"; //server
+$username = "root"; //username
+$password = ""; //password
+$dbname = "online_rest";  //database
+      $db = mysqli_connect($servername, $username, $password, $dbname);
+      $sql = "SELECT * FROM users ORDER BY u_id ASC";  
+      $result = mysqli_query($db, $sql);  
+      while($rows = mysqli_fetch_array($result))  
+      {       
+      $output .= '<tr>   
+                    <td>'.$rows['u_id'].'</td>                         
+                    <td>'.$rows['username'].'</td>
+                    <td>'.$rows['f_name'].'</td>
+                    <td>'.$rows['l_name'].'</td>
+                    <td>$'.$rows['email'].'</td>
+                    <td>$'.$rows['phone'].'</td>
+                    <td>'.$rows['address'].'</td>
+                    <td>'.$rows['date'].'</td>
+                </tr>';  
+      }  
+      return $output;  
+ }  
+if(isset($_POST["create_pdf"]))  
+ {  
+      require_once('tcpdf/tcpdf.php');  
+      $obj_pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);  
+      $obj_pdf->SetCreator(PDF_CREATOR);  
+      $obj_pdf->SetTitle("Zinnovare's Users Report");  
+      $obj_pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);  
+      $obj_pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));  
+      $obj_pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));  
+      $obj_pdf->SetDefaultMonospacedFont('helvetica');  
+      $obj_pdf->SetFooterMargin(PDF_MARGIN_FOOTER);  
+      $obj_pdf->SetMargins(PDF_MARGIN_LEFT, '15', PDF_MARGIN_RIGHT);  
+      $obj_pdf->setPrintHeader(false);  
+      $obj_pdf->setPrintFooter(false);  
+      $obj_pdf->SetAutoPageBreak(TRUE, 10);  
+      $obj_pdf->SetFont('helvetica', '', 10);  
+      $obj_pdf->AddPage();  
+      $content = '';  
+      $content .= '  
+      <h3 align="center">Zinnovare\'s Users Report</h3><br /><br />  
+      <table border="1" cellspacing="0" cellpadding="5">  
+           <tr>  
+                <th width="5%">ID</th>
+                <th width="12%">Username</th>
+                <th width="15%">FirstName</th>
+                <th width="15%">LastName</th>  
+                <th width="15%">Email</th>  
+                <th width="15%">Phone</th>  
+                <th width="12%">Address</th>
+                <th width="12%">Date</th>
+           </tr>  
+      ';  
+      $content .= fetch_data();  
+      $content .= '</table>';  
+      $obj_pdf->writeHTML($content);  
+      $obj_pdf->Output('sample.pdf', 'I');  
+ } 
 
 ?>
 <head>
@@ -229,6 +293,10 @@ session_start();
                                            
                                         </tbody>
                                     </table>
+                                    <br />  
+                     <form method="post">  
+                          <input type="submit" name="create_pdf" class="btn btn-danger" value="Download Report" />  
+                     </form> 
                                 </div>
                             </div>
                         </div>
