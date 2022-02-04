d07eec7b62552b04cadbc004cffa584ae405c8e7
sooyaaa0801 <72476689+sooyaaa0801@users.noreply.github.com>
2022-02-04 22:50:02 +0800

    login logout

--- login.php
+++ login.php
@@ -2,13 +2,13 @@
 <html lang="en" >
 
 <head>
-  <meta charset="UTF-8">
-  <title>login</title>
-  
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-
-  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
-<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+	<meta charset="UTF-8">
+		<title>Zinnovare Login</title>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+		<link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
+		<link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
+		<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+		<link rel="stylesheet" href="admin/css/login.css">
 
       <link rel="stylesheet" href="css/login.css">
 
@@ -60,32 +60,88 @@ if(isset($_POST['submit']))   // if button is submit
 <!-- Input Mixin-->
 <!-- Button Mixin-->
 <!-- Pen Title-->
-<div class="pen-title">
-  <h1>Login Form</h1>
-</div>
-<!-- Form Module-->
-<div class="module form-module">
-  <div class="toggle">
-   
-  </div>
-  <div class="form">
-    <h2>Login to your account</h2>
-	  <span style="color:red;"><?php echo $message; ?></span> 
-   <span style="color:green;"><?php echo $success; ?></span>
-    <form action="" method="post">
-      <input type="text" placeholder="Username"  name="username"/>
-      <input type="password" placeholder="Password" name="password"/>
-      <input type="submit" id="buttn" name="submit" value="login" />
-    </form>
-  </div>
+
+	  <div  class="headerPic">
+	<!--<button class="orderNow"><a href="admin/dashboard.php">Proceed</a></button>-->
+      <div class="headerMsg">
+        <div class="headTag">ZINNOVARE</div>
+        <div class="subTag">Login Page</div>
+		<div class="subsubTag">Login to your account to manage all the services.</div>
+		
+      </div>
+	  
+	  	<div style="margin-left:50%;">
+			<div style="padding-top: 8vw;padding-right:3vw;padding-bottom:2vw">
+			<div class="form">
+				<div style="margin-bottom: 2vw">
+					<img src="admin/images/login.png" width="100vw"/>
+				</div>
+				<!--<form class="register-form" action="index.php" method="post">
+					<input type="text" placeholder="username" name="cr_user"/>
+					<input type="text" placeholder="email address"  name="cr_email"/>
+					<input type="password" placeholder="password"  name="cr_pass"/>
+					<input type="password" placeholder="Confirm password"  name="cr_cpass"/>
+					<input type="password" placeholder="Unique-Code"  name="code"/>
+					<input type="submit"  name="submit1" value="Create" />
+					<p class="message">Already registered? <a href="#">Sign In</a></p>
+				</form>-->
+				<!--<span>username:admin</span>&nbsp;<span>password:1234</span>-->
+				
+				<span style="color:green;"><?php echo $success; ?></span>
+				<form class="login-form" action="" method="post">
+					<input style="background: #D9D9D9;" style="margin-top: 30px" type="text" placeholder="Username" name="username"/>
+					<input style="background: #D9D9D9;" type="password" placeholder="Password" name="password"/>
+					<input style="background-color: orange;" type="submit"  name="submit" value="Proceed" />
+					<!--<p class="message">Not registered? <a href="#">Create an account</a></p>-->
+				</form>
+				</div>
   
   <div class="cta">Not registered?<a href="registration.php" style="color:#f30;"> Create an account</a></div>
 </div>
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+				<span style="color:green;"><?php echo $message; ?></span>
+			</div>
+			
+			</div>
+			
+		</div>
+		<div class="footerCopyright">
+        <div class="row">
+
+          <div class="column">
+            <img src="admin/images/logo6.png" alt="Zinnovare" width="150px" height="40px"/>
+          </div>
+
+          <div class="column">
+            <a class="fa" href="https://www.facebook.com/zinnovare.finest">
+              <img alt="Facebook" src="admin/images/fb.png" width="30" height="30">
+            </a>
+            <a class="fa" href="https://www.instagram.com/zinnovare">
+              <img alt="instagram" src="admin/images/ig.png" width="30" height="30">
+            </a>
+            <a href=""><img alt="twitter" src="admin/images/twitter.png" width="30" height="30"></a>
+            <a href=""><img alt="youtube" src="admin/images/yt.png" width="30" height="30"></a>
+            <a href=""><img alt="linkedin" src="admin/images/linkedin.png" width="30" height="30"></a>
+          </div>
+          
+          <div class="column">
+            Copyright © 2017-2022
+            <span class="copyright">
+              Zinnovare Inc.
+            </span>
+          </div>
+
+        </div>
+      </div>
+		
+	</div>
 
   
 
-   
+	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+		<script src='js/index.js'></script>
+		<style>
+			<?php include "css/login.css" ?>
+		</style>
 
 
 
