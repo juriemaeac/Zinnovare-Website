6d3195f5faa0ff29fd0b6b2f4372cd31f17acca6
sooyaaa0801 <72476689+sooyaaa0801@users.noreply.github.com>
2022-03-01 00:37:12 +0800

    aa

--- admin/all_orders.php
+++ admin/all_orders.php
@@ -332,7 +332,7 @@
                                                             echo '	<td>'.$rows['date'].'</td>';
                                                             ?>
                                                                 <td>
-                                                                <a href="delete_orders.php?order_del=<?php echo $rows['o_id'];?>" onclick="return confirm('Are you sure?');" class="btn btn-danger btn-flat btn-addon btn-xs m-b-10" style="background-color: red;"><i class="fa fa-trash-o" style="font-size:16px"></i></a> 
+                                                                 
                                                             <?php
                                                             echo '<a href="view_order.php?user_upd='.$rows['o_id'].'" " class="btn btn-info btn-flat btn-addon btn-sm m-b-10 m-l-5"><i class="ti-settings"></i></a>
                                                                 </td>
